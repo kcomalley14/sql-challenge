c34f67d37cc66809ba9a8bd5ea61585d66a70a6b
Kyle O'Malley <kyleomalley@Kyles-MBP-303.lan>
2020-11-10 22:24:25 -0500

    updated tableschema order

--- tableschema.sql
+++ tableschema.sql
@@ -1,9 +1,9 @@
 DROP TABLE Departments CASCADE;
+DROP TABLE Titles CASCADE;
+DROP TABLE Employees CASCADE;
 DROP TABLE Dept_employees CASCADE;
 DROP TABLE Dept_managers CASCADE;
-DROP TABLE Employees CASCADE;
 DROP TABLE Salaries CASCADE;
-DROP TABLE Titles CASCADE;
 
 CREATE TABLE Departments (
     dept_no VARCHAR(30)   NOT NULL,
@@ -12,6 +12,28 @@ CREATE TABLE Departments (
 );
 
 SELECT * FROM Departments;
+
+CREATE TABLE Titles (
+    title_id VARCHAR(30)   NOT NULL,
+    title VARCHAR(30)   NOT NULL,
+    	PRIMARY KEY (title_id)
+);
+
+SELECT * FROM Titles;
+
+CREATE TABLE Employees (
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR(30)   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(30)   NOT NULL,
+    last_name VARCHAR(30)   NOT NULL,
+    sex VARCHAR(30)   NOT NULL,
+    hire_date DATE   NOT NULL,
+    	PRIMARY KEY (emp_no),
+		FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
+);
+
+SELECT * FROM Employees;
 
 CREATE TABLE Dept_employees (
     emp_no INT   NOT NULL,
@@ -31,20 +53,6 @@ CREATE TABLE Dept_managers (
 
 SELECT * FROM Dept_managers;
 
-CREATE TABLE Employees (
-    emp_no INT   NOT NULL,
-    emp_title_id VARCHAR(30)   NOT NULL,
-    birth_date DATE   NOT NULL,
-    first_name VARCHAR(30)   NOT NULL,
-    last_name VARCHAR(30)   NOT NULL,
-    sex VARCHAR(30)   NOT NULL,
-    hire_date DATE   NOT NULL,
-    	PRIMARY KEY (emp_no),
-		FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
-);
-
-SELECT * FROM Employees;
-
 CREATE TABLE Salaries (
     emp_no INT   NOT NULL,
     salary INT   NOT NULL,
@@ -52,11 +60,3 @@ CREATE TABLE Salaries (
 );
 
 SELECT * FROM Salaries;
-
-CREATE TABLE Titles (
-    title_id VARCHAR(30)   NOT NULL,
-    title VARCHAR(30)   NOT NULL,
-    	PRIMARY KEY (title_id)
-);
-
-SELECT * FROM Titles;
